61ebb45a64946c802ddeaef67dfa28206b9ae2a2
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-26 19:53:48 +0530

    Create great.java

---
+++ beginn/great.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,b,c,d;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		d=((a>b)?((a>c)?a:c):((b>c)?b:c));
+		System.out.println(d);
+	}
+}
