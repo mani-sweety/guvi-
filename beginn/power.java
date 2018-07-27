8948c386899b2c7a8767f14687ed61e35f367ddf
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-27 10:52:07 +0530

    Create power.java

---
+++ beginn/power.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,k,i,mul;
+	mul=1;
+		Scanner sc=new Scanner(System.in);
+	
+		n=sc.nextInt();
+		i=sc.nextInt();
+		for(k=1;k<=i;k++){
+			mul=mul*n;
+		
+			
+		}
+		System.out.println(mul);
+		
+	}
+}
