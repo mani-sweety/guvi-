4ebbcb1904698350ef27b1459407436149071414
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-27 16:06:26 +0530

    Create odd.java

---
+++ beginn/odd.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int m,n,i;
+		Scanner sc=new Scanner(System.in);
+		m=sc.nextInt();
+		n=sc.nextInt();
+		for(i=(m+1);i<n;i++){
+			if((i%2)==0){
+		
+		
+			
+			}
+			else{
+				
+			System.out.print(i);
+			
+			}
+			System.out.print(" ");
+			
+		}
+		
+		
+	}
+}
