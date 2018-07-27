2c0510ebcf8b86c243a4007fb689f35288e35cae
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-27 12:38:47 +0530

    Create primes.java

---
+++ beginn/primes.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,a,m,j,i;
+	
+	
+		Scanner sc=new Scanner(System.in);
+		m=sc.nextInt();
+		n=sc.nextInt();
+		
+		for(i=m;i<n;i++){
+			a=0;
+			for(j=2;j<i;j++){
+		
+			if((i%j)==0){
+                        	++a;
+                        	break;
+			}
+		}
+                        	if(a==0){
+                        	System.out.println(i);
+                        
+                        	
+		
+			}
+	}
+			
+		
+		
+	}
+}
