722ae134be239395e9e11c159d131d1887f12c85
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-28 20:01:40 +0530

    Update odd.java

--- beginn/odd.java
+++ beginn/odd.java
@@ -8,23 +8,25 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		int m,n,i;
+		int m,n,i,k;
+		k=0;
 		Scanner sc=new Scanner(System.in);
 		m=sc.nextInt();
 		n=sc.nextInt();
 		for(i=(m+1);i<n;i++){
 			if((i%2)==0){
-		
-		
-			
 			}
 			else{
-				
-			System.out.print(i);
-			if((n-m)>2)
+			k++;	
+			if(k==1)
 			{
-			System.out.print(" ");
+			System.out.print(i);
 			}
+			else{
+			System.out.print(" ");
+			System.out.print(i);
+			}
+			
 			}
 			
 		}
