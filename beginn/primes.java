bb4b8f576c8bc3ad6570b9f599ddbe815e714e78
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-29 10:45:50 +0530

    Update primes.java

--- beginn/primes.java
+++ beginn/primes.java
@@ -8,28 +8,38 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		int n,a,m,j,i;
+		int n,a,m,j,i,k;
+		k=0;
 	
 	
 		Scanner sc=new Scanner(System.in);
 		m=sc.nextInt();
 		n=sc.nextInt();
 		
-		for(i=m;i<n;i++){
+		for(i=(m+1);i<n;i++){
 			a=0;
 			for(j=2;j<i;j++){
 		
 			if((i%j)==0){
                         	++a;
-                        	break;
+                        	
 			}
 		}
                         	if(a==0){
-                        	System.out.print(i+" ");
-                        
-                        	
-		
-			}
+					
+                        		if(k==0){
+                        	System.out.print(i);
+                        			
+                        		}
+                        	else{
+                        		System.out.print(" ");
+                        		System.out.print(i);
+                        		
+                        		
+				}
+					++k;
+				}
+			
 	}
 			
 		
