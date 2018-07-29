6c215872ff159ab7d43eb95e326e8f7f37812874
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-29 16:23:01 +0530

    Create sumap.java

---
+++ beginn/sumap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes 
+		Scanner sc = new Scanner(System.in);
+   
+    int n=sc.nextInt(),a=sc.nextInt(),d=sc.nextInt();
+    System.out.print(n*(2*a+(n-1)*d)/2);
+   
+	}
+}
