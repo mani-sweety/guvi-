89763343b3fa975300f89bf243a24751f6338846
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-07-30 19:12:06 +0530

    Create minarray.java

---
+++ beginn/minarray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i,j,min,n;
+		int[] a=new int[10];
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(i=0;i<n;i++){
+			a[i]=sc.nextInt();
+			
+		}
+		min=a[0];
+		for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(a[j]<min){
+				min=a[j];
+			}
+	}
+		}
+		
+	System.out.print(min);
+	
+}
+}
