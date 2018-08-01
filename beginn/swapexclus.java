15664435577d5d16e30de9abc6dc9034a3d30630
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-01 14:24:06 +0530

    Create swapexclus.java

---
+++ beginn/swapexclus.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1,n2;
+		Scanner sc=new Scanner(System.in);
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		n1=n1^n2;
+		n2=n1^n2;
+		n1=n1^n2;
+		System.out.print(n1+" "+n2);
+		
+	}
+}
