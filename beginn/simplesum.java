6c19b320b28e3835debd569f6b51833ec8d45adc
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-02 15:09:33 +0530

    Create simplesum.java

---
+++ beginn/simplesum.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1,n2,sum=0;
+		
+		Scanner sc=new Scanner(System.in);
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		System.out.print(n1+n2);
+	}
+}
