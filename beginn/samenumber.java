efacbcac80def6fcb6a6f17ce851ae9ebdd3b4ea
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-03 13:48:55 +0530

    Create samenumber.java

---
+++ beginn/samenumber.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,rem=0,i,m=0;
+		int[] a=new int[10];
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+	                    i=0;
+			while(n>=1){
+			
+				rem=n%10;
+				
+				a[i]=rem;
+				
+				n=n/10;
+				i++;
+			}
+		
+		
+                                   	while(i>0){
+			--i;	
+			System.out.print(a[i]);
+			if(i>=1)
+			  System.out.print(" ");
+			}
+		
+		
+	}
+}
