49688b74e50122c9721d1853d9252f2eee86b386
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-03 14:48:02 +0530

    Create muleven.java

---
+++ beginn/muleven.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,m,mul;
+		Scanner sc=new Scanner(System.in);
+		m=sc.nextInt();
+		n=sc.nextInt();
+		mul=m*n;
+	                    if(mul%2==0){
+			System.out.print("even");}
+			else{
+				System.out.print("odd");
+			}
+	}
+}
