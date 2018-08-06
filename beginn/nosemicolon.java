43b27457944c3b8e6ce8d38d9aa3b62064abae73
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-06 14:19:55 +0530

    Create nosemicolon.java

---
+++ beginn/nosemicolon.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		for(int n=1;n<2; System.out.print(s)){
+			n++;
+		}
+			
+		}
+}
