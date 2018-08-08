e706032eab854045b3b75fcc7fb4dc08f2a94f77
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-08 13:47:18 +0530

    Create kthodd.java

---
+++ beginn/kthodd.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			int n,m,k=0,l=0;
+		int[] a=new int[10];
+		Scanner sc=new Scanner(System.in);
+		m=sc.nextInt();
+		n=sc.nextInt();
+		for(int j=0;j<m;j++){
+			a[j]=sc.nextInt();
+		}
+		
+		for(int i=0;i<m;i++){
+			if((a[i]%2)==0){
+				
+			}
+			else{
+				k=a[i];
+			
+				l++;
+				if(l==n){
+			          System.out.print(k);
+				}
+			}
+			
+			
+		}
+	
+	
+	}
+}
