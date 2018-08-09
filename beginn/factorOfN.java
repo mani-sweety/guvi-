42bd94ab28ea2756b7933ead21a9f01e5fabd3e2
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-09 19:54:21 +0530

    Update factorOfN.java

--- beginn/factorOfN.java
+++ beginn/factorOfN.java
@@ -17,10 +17,12 @@ class Ideone
 		for(int i=1;i<=n;i++)
 		{
 		if((n%i)==0){
-			System.out.print(i+" ");
-		}
-		else{
-			
+			if(i==1){
+			System.out.print(i);
+			}
+			else{
+				System.out.print(" "+i);
+			}
 		}
 		}
 	}
