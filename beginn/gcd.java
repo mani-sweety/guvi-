ed2c1a207edb3f9ac9f01bce24f6cdfe450a7686
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-12 19:42:25 +0530

    Create gcd.java

---
+++ beginn/gcd.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int m,n,k=0;
+		Scanner sc=new Scanner(System.in);
+		m=sc.nextInt();
+		n=sc.nextInt();
+		for(int i=1;i<=m&&i<=n;i++){
+			if((m%i==0)&&(n%i==0)){
+				k=i;
+			}
+		}
+		System.out.print(k);
+	}
+}
