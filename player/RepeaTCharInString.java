1bdb7be62db9766e579cf11bdd92a970ee7db96d
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-08-16 12:50:31 +0530

    Create RepeaTCharInString.java

---
+++ player/RepeaTCharInString.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str;
+		Scanner sc=new Scanner(System.in);
+		str=sc.nextLine();
+		char[] a=str.toCharArray();
+     char d=' ';
+		for(int i=0;i<a.length-1;i++){  
+			if(a[i]==a[i+1]){
+			d=a[i+1];
+			}
+		}
+		
+		System.out.print(d);
+	}
+}
